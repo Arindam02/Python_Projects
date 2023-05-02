735d44bd12c20f33d193f8da4e14870d956ca414
Arindam02 <89266388+Arindam02@users.noreply.github.com>
2023-05-02 22:15:41 +0530

    Snake game add

---
+++ Snake_Game/scorebord.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over" , align="center", font=("Arial", 40, "normal"))
+
+    def increse_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
